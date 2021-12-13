c00fa85f9cd9eaad4e69ba84d6a6c3df3b222334
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2021-12-13 15:21:12 -0300

    alteração para criar uma comida em lugar aleatorio antes do inicio

--- app/Main.hs
+++ app/Main.hs
@@ -8,4 +8,5 @@ main :: IO ()
 main = do
     let mundo = mundoInicial 
     stdGen <- getStdGen 
-    play (InWindow "hSnake" (tamJanela , tamJanela) (500, 500)) white 60 (mundo { randomGen = stdGen }) desenhaMundo tratarEvento atualizaMundo
+    let ((x,y), gen2) = novaComida (cobra mundo) stdGen
+    play (InWindow "hSnake" (tamJanela , tamJanela) (500, 500)) white 60 (mundo { randomGen = gen2, comida = (x,y) }) desenhaMundo tratarEvento atualizaMundo
